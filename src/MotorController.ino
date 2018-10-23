aadcfa22fbbb894807443256fbb7f71afa7476f8
ohmiclaser <ohmiclaser@gmail.com>
2018-10-23 14:34:24 +0100

    working code

--- src/MotorController.ino
+++ src/MotorController.ino
@@ -3,9 +3,10 @@
 const int readPinA = A0;
 const int readPinB = A1;
 
-const int pwmPin = 3;
+const int pwnPin = 3;
 const int posDirectionPin = 4;
 const int negDirectionPin = 5;
+const int8_t threshold = 5;
 
 void setup(){
     Serial.begin(9600);
@@ -15,11 +16,25 @@ void setup(){
 void loop(){
     int a0 = analogRead(A0);
     int a1 = analogRead(A1);
-    if( a0+a1 > 0 ){
-        Serial.print(a0);
-        Serial.print(", ");
-        Serial.print(a1);
-        Serial.println("");
+    if( a0+a1 < threshold ){
+        digitalWrite(posDirectionPin, LOW);
+        digitalWrite(negDirectionPin, LOW);
+        digitalWrite(pwnPin, LOW);  
     }    
+    else if(a0>a1){
+        digitalWrite(posDirectionPin, HIGH);
+        Serial.println("pos");
+        digitalWrite(negDirectionPin, LOW);
+        analogWrite(pwnPin, 255);
+    }
+    else if(a0<a1){
+        digitalWrite(posDirectionPin, LOW);
+        digitalWrite(negDirectionPin, HIGH);
+        Serial.println("neg");
+        analogWrite(pwnPin, 255);
+    }
+   
+
+    
     delay(10);
 }
