fd6f56bc035a024050e1446ea30e6d7c2923cacf
Paul Hayes <paul.hayes@entropedia.co.uk>
2018-10-23 13:35:40 +0100

    Added initial wiring

--- src/MotorController.ino
+++ src/MotorController.ino
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
-void setup(){
+const int readPinA = A0;
+const int readPinB = A1;
 
+const int pwmPin = 3;
+const int posDirectionPin = 4;
+const int negDirectionPin = 5;
+
+void setup(){
+    Serial.begin(9600);
+       
 }
 
 void loop(){
-
+    int a0 = analogRead(A0);
+    int a1 = analogRead(A1);
+    if( a0+a1 > 0 ){
+        Serial.print(a0);
+        Serial.print(", ");
+        Serial.print(a1);
+        Serial.println("");
+    }    
+    delay(10);
 }
