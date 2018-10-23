156d32f1f0570f1d70c9ee054996fc749c211ad6
Paul Hayes <paul.hayes@entropedia.co.uk>
2018-10-23 19:06:43 +0100

    Added low pass filter to filter out external noise from the analog input. Also using max instead of addition when evaluating signals against threshold

--- src/MotorController.ino
+++ src/MotorController.ino
@@ -6,7 +6,10 @@ const int readPinB = A1;
 const int pwnPin = 3;
 const int posDirectionPin = 4;
 const int negDirectionPin = 5;
-const int8_t threshold = 5;
+const int8_t threshold = 50;
+
+int a0 = 0;
+int a1 = 0;
 
 void setup(){
     Serial.begin(9600);
@@ -14,12 +17,16 @@ void setup(){
 }
 
 void loop(){
-    int a0 = analogRead(A0);
-    int a1 = analogRead(A1);
-    if( a0+a1 < threshold ){
+    a0 = a0 + 10*( analogRead(A0) - a0 ) / 100;
+    a1 = a1 + 10*( analogRead(A1) - a1 )  / 100;
+
+
+
+    if( max(a0,a1) < threshold ){
         digitalWrite(posDirectionPin, LOW);
         digitalWrite(negDirectionPin, LOW);
         digitalWrite(pwnPin, LOW);  
+
     }    
     else if(a0>a1){
         digitalWrite(posDirectionPin, HIGH);
